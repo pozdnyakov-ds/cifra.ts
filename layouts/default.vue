b7a0bbc352c12b428445775cb883973958ab21b4
Dmitry Pozdnyakov <2903015@gmail.com>
2022-12-18 14:56:55 +0300

    v.0

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>Header</p>
+    <Nuxt />
+    <p>Footer</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>Wellcome!</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+let name: string = ''
+let count = ref(0)    
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
